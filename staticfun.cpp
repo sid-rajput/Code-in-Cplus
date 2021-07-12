d86d4818d5b7bd730f6d41763d006d181ce7d794
sid-rajput <86182533+sid-rajput@users.noreply.github.com>
2021-07-13 00:14:41 +0530

    static function

---
+++ staticfun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+class box
+{
+   public:
+        static int count;
+     cout<<"count"<<;
+     count++
+
+}
+int main()
+{
+    for (int i=0; i<5; i++)
+        demo();
+    return 0;
+}
